4eb31a61cd59fe642d6e57fbeab92b10717dc6bf
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2023-08-11 10:33:53 -0400

    Initial Commit

---
+++ HomeSchool/HomeSchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeSchoolApp.swift
+//  HomeSchool
+//
+//  Created by Tarun Sachdeva on 2023-08-11.
+//
+
+import SwiftUI
+
+@main
+struct HomeSchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
